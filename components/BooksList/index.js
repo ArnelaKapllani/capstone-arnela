6df659a7527d451eb719a0d22b8648af4e33f5f9
arnelakapllani <arnela.kapllani@web.de>
2023-07-09 01:25:18 +0200

    add styles to the list

--- components/BooksList/index.js
+++ components/BooksList/index.js
@@ -1,13 +1,22 @@
 import Book from "../Book/index.js";
 import { books } from "../../lib/books.js";
+import styled from "styled-components";
+
+const StyledList = styled.div`
+  display: grid;
+  gap: 20px;
+  padding-bottom: 25px;
+`;
 
 export default function BooksList() {
   return (
     <>
       <h2>List of Books:</h2>
-      {books.map((book) => (
-        <Book key={book.id} book={book} />
-      ))}
+      <StyledList>
+        {books.map((book) => (
+          <Book key={book.id} book={book} />
+        ))}
+      </StyledList>
     </>
   );
 }
