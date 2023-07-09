a0c34b5981c414072266bde8e100ff447aa8fe5a
arnelakapllani <arnela.kapllani@web.de>
2023-07-09 11:03:33 +0200

    pass the book as prop to the Book component

--- components/Book/index.js
+++ components/Book/index.js
@@ -15,7 +15,7 @@ export default function Book({ book }) {
   return (
     <div>
       <Image src={imageURL} alt={title} width={150} height={200} />
-      {/* <h3>{book.ISBN}</h3>
+      {/* <h3>{ISBN}</h3>
       <h3>{EAN}</h3>
       <h3>{language}</h3>
       <h3>{pagenumber}</h3>
