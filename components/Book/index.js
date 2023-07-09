792eba97ae575ca7b69455246dbf2038c0a54682
arnelakapllani <arnela.kapllani@web.de>
2023-07-09 10:58:20 +0200

    destructure the book prop

--- components/Book/index.js
+++ components/Book/index.js
@@ -1,16 +1,27 @@
 import Image from "next/image";
 
 export default function Book({ book }) {
+  const {
+    imageURL,
+    ISBN,
+    EAN,
+    language,
+    pagenumber,
+    publication,
+    price,
+    description,
+  } = book;
+
   return (
     <div>
-      <Image src={book.imageURL} alt={book.title} width={150} height={200} />
+      <Image src={imageURL} alt={title} width={150} height={200} />
       {/* <h3>{book.ISBN}</h3>
-      <h3>{book.EAN}</h3>
-      <h3>{book.language}</h3>
-      <h3>{book.pagenumber}</h3>
-      <h3>{book.publication}</h3>
-      <h3>{book.price}</h3>
-      <p>{book.description}</p> */}
+      <h3>{EAN}</h3>
+      <h3>{language}</h3>
+      <h3>{pagenumber}</h3>
+      <h3>{publication}</h3>
+      <h3>{price}</h3>
+      <p>{description}</p> */}
     </div>
   );
 }
