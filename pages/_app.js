ee91158423b004f0f171539603883cce61a5f14d
arnelakapllani <arnela.kapllani@web.de>
2023-07-13 23:43:18 +0200

    pass the book.js to the app file

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import GlobalStyle from "../styles";
+import { books } from "../lib/books.js";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} books={books} />
     </>
   );
 }
