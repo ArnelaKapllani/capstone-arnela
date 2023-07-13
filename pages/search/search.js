ac04d3cab5b670d71819d06fb61a7277abbab39c
arnelakapllani <arnela.kapllani@web.de>
2023-07-13 23:42:18 +0200

    pass SearchInput component to Search page

---
+++ pages/search/search.js
@@ -0,0 +1,10 @@
+import SearchInput from "../../components/SearchInput/index.js";
+import BookImage from "../../components/BookImage/index.js";
+
+export default function SearchInputPage() {
+  return (
+    <div>
+      <SearchInput />
+    </div>
+  );
+}
