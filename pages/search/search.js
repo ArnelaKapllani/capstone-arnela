73717c71bf4fbea8300505c82469b6a899e81b0d
arnelakapllani <arnela.kapllani@web.de>
2023-07-13 23:53:14 +0200

    create ShoppingCartButton component

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,10 +1,12 @@
 import SearchInput from "../../components/SearchInput/index.js";
 import BookImage from "../../components/BookImage/index.js";
+import ShoppingCartButton from "@/components/ShoppingCartButton/index.js";
 
 export default function SearchInputPage() {
   return (
     <div>
       <SearchInput />
+      <ShoppingCartButton />
     </div>
   );
 }
