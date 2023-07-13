7a137b0deb9f3fb438ae02cdcacf7c758caa2679
arnelakapllani <arnela.kapllani@web.de>
2023-07-14 00:10:08 +0200

    create Bookmark component

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,12 +1,13 @@
 import SearchInput from "../../components/SearchInput/index.js";
-import BookImage from "../../components/BookImage/index.js";
 import ShoppingCartButton from "@/components/ShoppingCartButton/index.js";
+import BookmarkButton from "@/components/BookmarkButton/index.js";
 
 export default function SearchInputPage() {
   return (
     <div>
       <SearchInput />
       <ShoppingCartButton />
+      <BookmarkButton />
     </div>
   );
 }
