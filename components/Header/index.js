18ac17df4ef4500cf4fda776c2510c65e9376a0a
arnelakapllani <arnela.kapllani@web.de>
2023-07-15 22:50:44 +0200

    create Header component

---
+++ components/Header/index.js
@@ -0,0 +1,13 @@
+import DarkModeButton from "../DarkModeButton";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        YourBook
+        <p>Books are portable magic!</p>
+      </header>
+      <DarkModeButton />
+    </>
+  );
+}
