e22e53e8fdc27e08f93740440d1c8417e23dbf29
arnelakapllani <arnela.kapllani@web.de>
2023-07-15 22:48:22 +0200

    pass Layout component to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import GlobalStyle from "../styles";
+import Layout from "../components/Layout/index.js";
 import { books } from "../lib/books.js";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} books={books} />
+      <Layout>
+        <Component {...pageProps} books={books} />
+      </Layout>
     </>
   );
 }
