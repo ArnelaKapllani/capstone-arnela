b045fda4000a10c75ba4df9f0c7d702ea1aaf9ac
arnelakapllani <arnela.kapllani@web.de>
2023-07-18 13:08:05 +0200

    implement the buttons for each result

--- components/ShoppingCart/index.js
+++ components/ShoppingCart/index.js
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
 import BookImage from "../BookImage";
 import BookmarkButton from "../BookmarkButton";
 import DropdownButton from "../DropdownButton";
-import DeleteButton from "../DeleteButton";
+import DeleteButton from "../DeleteButton/index.js";
 
-export default function ShoppingCart({ cartItems }) {
+export default function ShoppingCart({ cartItems, handleDelete }) {
   return (
     <>
       <h1>My cart</h1>
@@ -12,14 +11,14 @@ export default function ShoppingCart({ cartItems }) {
         cartItems.map((book) => (
           <div key={book.id}>
             <BookImage book={book} />
+            <DropdownButton />
+            <DeleteButton book={book} onDelete={handleDelete} />
+            <BookmarkButton />
           </div>
         ))
       ) : (
         <p>Your cart is empty.</p>
       )}
-      <DropdownButton />
-      <DeleteButton />
-      <BookmarkButton />
       <p>Total:</p>
       <button type="button">Next</button>
     </>
