681f0867304b414392348d2526090b1bafc1e4a2
arnelakapllani <arnela.kapllani@web.de>
2023-07-18 13:14:37 +0200

    modify addToCart function

--- components/ShoppingCartButton/index.js
+++ components/ShoppingCartButton/index.js
@@ -3,12 +3,9 @@ import Icon from "@mdi/react";
 import { mdiBasketOutline } from "@mdi/js";
 
 export default function ShoppingCartButton({ book, addToCart }) {
-  const handleAddToCart = () => {
-    addToCart(book);
-  };
   return (
     <>
-      <button type="button" onClick={handleAddToCart}>
+      <button type="button" onClick={() => addToCart(book)}>
         <Icon path={mdiBasketOutline} size={1} />
       </button>
     </>
