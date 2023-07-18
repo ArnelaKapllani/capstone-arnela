54c39f8df19e8419e96dd3ef9869d2fb5f8a7b37
arnelakapllani <arnela.kapllani@web.de>
2023-07-18 03:09:58 +0200

    modify app.js to handle the shopping cart state

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,29 @@
 import GlobalStyle from "../styles";
+import { useState } from "react";
+import SearchInput from "../components/SearchInput/index.js";
+import ShoppingCart from "../components/ShoppingCart/index.js";
 import { books } from "../lib/books.js";
 
 export default function App({ Component, pageProps }) {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (book) => {
+    const updateCartItems = [...cartItems, book];
+    console.log(updateCartItems);
+    setCartItems(updateCartItems);
+  };
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} books={books} />
+      <Component
+        {...pageProps}
+        books={books}
+        addToCart={addToCart}
+        cartItems={cartItems}
+      />
+      {/* <SearchInput addToCart={addToCart} />
+      <ShoppingCart cartItems={cartItems} /> */}
     </>
   );
 }
