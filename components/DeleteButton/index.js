d9314f896c739a2071647b949ef77baf6b18855c
arnelakapllani <arnela.kapllani@web.de>
2023-07-18 02:56:20 +0200

    create components Delete and Dropdown Buttons

---
+++ components/DeleteButton/index.js
@@ -0,0 +1,7 @@
+export default function DeleteButton() {
+  return (
+    <>
+      <button type="button">Delete</button>
+    </>
+  );
+}
---
+++ components/DropdownButton/index.js
@@ -0,0 +1,7 @@
+export default function DropdownButton() {
+  return (
+    <>
+      <button type="button">Dropdown</button>
+    </>
+  );
+}
