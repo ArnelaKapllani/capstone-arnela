03d34224ac0c1dc5827fdf95c8b4fa54f3293a93
arnelakapllani <arnela.kapllani@web.de>
2023-07-18 03:01:48 +0200

    create ShoppingCart component

---
+++ components/ShoppingCart/index.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import BookImage from "../BookImage";
+import BookmarkButton from "../BookmarkButton";
+import DropdownButton from "../DropdownButton";
+import DeleteButton from "../DeleteButton";
+
+export default function ShoppingCart({ cartItems }) {
+  //   const [cartItems, setCartItems] = useState([]);
+
+  return (
+    <>
+      <h1>My cart</h1>
+      {cartItems && cartItems.length > 0 ? (
+        cartItems.map((book) => (
+          <div key={book.id}>
+            <BookImage book={book} />
+          </div>
+        ))
+      ) : (
+        <p>Your cart is empty.</p>
+      )}
+      <DropdownButton />
+      <DeleteButton />
+      <BookmarkButton />
+      <p>Total:</p>
+      <button type="button">Next</button>
+    </>
+  );
+}
