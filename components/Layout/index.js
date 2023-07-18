667abc4140b969b1a49c6c73ef4a9a8bba202be9
arnelakapllani <arnela.kapllani@web.de>
2023-07-18 13:05:50 +0200

    pass cartItems as a prop to Layout component

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,12 +1,12 @@
 import Header from "../Header";
 import NavigationBar from "../NavigationBar";
 
-export default function Layout({ children }) {
+export default function Layout({ children, cartItems }) {
   return (
     <>
       <Header />
       <main>{children}</main>
-      <NavigationBar />
+      <NavigationBar cartItems={cartItems} />
     </>
   );
 }
