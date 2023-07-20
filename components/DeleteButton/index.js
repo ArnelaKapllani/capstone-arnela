b63e1ead8b25d0c5a5b4e382f0a44fb0e0825867
arnelakapllani <arnela.kapllani@web.de>
2023-07-21 01:49:29 +0200

    change the name of the function

--- components/DeleteButton/index.js
+++ components/DeleteButton/index.js
@@ -2,12 +2,12 @@ import Icon from "@mdi/react";
 import { mdiDeleteOutline } from "@mdi/js";
 
 export default function DeleteButton({ book, onDelete }) {
-  function handleDelete() {
-    onDelete(book);
+  function removeFromCart() {
+    onDelete(book.id);
   }
   return (
     <>
-      <button type="button" onClick={handleDelete}>
+      <button type="button" onClick={removeFromCart}>
         <Icon path={mdiDeleteOutline} size={1} />
       </button>
     </>
