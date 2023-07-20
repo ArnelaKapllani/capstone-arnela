ae4a4f8b09b63d50c921ca7f31f7989ba1fc99f0
arnelakapllani <arnela.kapllani@web.de>
2023-07-21 01:50:11 +0200

    create DropdownButton component

--- components/DropdownButton/index.js
+++ components/DropdownButton/index.js
@@ -1,7 +1,29 @@
-export default function DropdownButton() {
+import { useState, useEffect } from "react";
+
+export default function DropdownButton({
+  book,
+  quantity,
+  updateQuantityAndPrice,
+}) {
+  const [selectedValue, setSelectedValue] = useState(quantity);
+
+  useEffect(() => {
+    setSelectedValue(quantity);
+  }, [quantity]);
+
+  function handleSelectedChange(event) {
+    const newQuantity = parseInt(event.target.value);
+    setSelectedValue(newQuantity);
+
+    updateQuantityAndPrice(book.id, newQuantity);
+  }
   return (
-    <>
-      <button type="button">Dropdown</button>
-    </>
+    <select value={selectedValue} onChange={handleSelectedChange}>
+      <option value={1}>1</option>
+      <option value={2}>2</option>
+      <option value={3}>3</option>
+      <option value={4}>4</option>
+      <option value={5}>5</option>
+    </select>
   );
 }
