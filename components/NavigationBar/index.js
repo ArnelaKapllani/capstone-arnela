76a7789200ce8de7eea826e4db32029ff75a2888
arnelakapllani <arnela.kapllani@web.de>
2023-07-21 01:52:04 +0200

    add function to calculate the total quantity of each book in the cartItems array and stores the result in the bookQuantities object

--- components/NavigationBar/index.js
+++ components/NavigationBar/index.js
@@ -8,6 +8,16 @@ import {
 } from "@mdi/js";
 
 export default function NavigationBar({ cartItems }) {
+  const bookQuantities = cartItems.reduce((accumulator, cartItem) => {
+    const bookId = cartItem.bookId;
+    if (accumulator[bookId]) {
+      accumulator[bookId] += cartItem.quantity;
+    } else {
+      accumulator[bookId] = cartItem.quantity;
+    }
+    return accumulator;
+  }, {});
+
   return (
     <footer>
       <NavBar>
@@ -25,7 +35,14 @@ export default function NavigationBar({ cartItems }) {
         </StyledIcon>
         <StyledIcon href="/shopping-cart" aria-label="see shopping cart">
           <Icon path={mdiBasketOutline} size={1} />
-          {cartItems.length > 0 && <span>{cartItems.length}</span>}
+          {cartItems.length > 0 && (
+            <span>
+              {Object.keys(bookQuantities).reduce(
+                (total, bookId) => total + bookQuantities[bookId],
+                0
+              )}
+            </span>
+          )}
           Shopping Cart
         </StyledIcon>
       </NavBar>
