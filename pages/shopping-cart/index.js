3724460181f624dc7a8a41451f314581de319018
arnelakapllani <arnela.kapllani@web.de>
2023-07-21 01:45:44 +0200

    change the prop in the ShoppingCart component

--- pages/shopping-cart/index.js
+++ pages/shopping-cart/index.js
@@ -1,11 +1,11 @@
 import Layout from "../../components/Layout/index.js";
 import ShoppingCart from "../../components/ShoppingCart/index.js";
 
-export default function ShoppingCartPage({ cartItems, handleDelete }) {
+export default function ShoppingCartPage({ cartItems, removeFromCart }) {
   return (
     <>
       <Layout cartItems={cartItems} />
-      <ShoppingCart cartItems={cartItems} handleDelete={handleDelete} />
+      <ShoppingCart cartItems={cartItems} removeFromCart={removeFromCart} />
     </>
   );
 }
